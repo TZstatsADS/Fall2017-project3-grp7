a8cd07177e8f09dfe5ce112e9daabc8e304cb395
Lin Han <lh2810@columbia.edu>
2017-10-30 21:23:16 -0400

    LINH

---
+++ lib/feature.R
@@ -0,0 +1,51 @@
+#############################################################
+### Construct visual features for training/testing images ###
+#############################################################
+
+### Authors: Lin Han
+### Project 3
+### ADS Fall2017_GROUP7
+
+feature <- function(img_dir, export=T){
+  
+  ### Construct process features for training/testing images
+  ### Sample simple feature: Extract row average raw pixel values as features
+  
+  ### Input: a directory that contains images ready for processing
+  ### Output: an .RData file contains processed features for the images
+  
+  ### load libraries
+  library("EBImage")
+  library(grDevices)
+  ### Define the b=number of R, G and B
+  nR <- 10
+  nG <- 12
+  nB <- 12 
+  rBin <- seq(0, 1, length.out=nR)
+  gBin <- seq(0, 1, length.out=nG)
+  bBin <- seq(0, 1, length.out=nB)
+  mat=array()
+  freq_rgb=array()
+  rgb_feature=matrix(nrow=3000, ncol=nR*nG*nB)
+  
+  n_files <- length(list.files(img_dir))
+  
+########extract RGB features############
+  for (i in 1:3000){
+    mat <- imageData(readImage(paste0(img_dir, "img", "_", sprintf("%04.f",i), ".jpg")))
+    mat_as_rgb <-array(c(mat,mat,mat),dim = c(nrow(mat),ncol(mat),3))
+    freq_rgb <- as.data.frame(table(factor(findInterval(mat_as_rgb[,,1], rBin), levels=1:nR), 
+                                    factor(findInterval(mat_as_rgb[,,2], gBin), levels=1:nG),
+                                    factor(findInterval(mat_as_rgb[,,3], bBin), levels=1:nB)))
+    rgb_feature[i,] <- as.numeric(freq_rgb$Freq)/(ncol(mat)*nrow(mat)) # normalization
+    
+    mat_rgb <-mat_as_rgb
+    dim(mat_rgb) <- c(nrow(mat_as_rgb)*ncol(mat_as_rgb), 3)
+  }
+  
+  ### output constructed features
+  if(export){
+    saveRDS(rgb_feature, file = "../output/rgb_feature_new.RData")
+  }
+  return(rgb_feature)
+}
